39bbdb289c63748bdd1a661d20cc93a16676f9de
chaim <chchdeu@gmail.com>
2026-06-21 17:28:47 +0300

    cave

--- advenure_trip.py
+++ advenure_trip.py
@@ -1,6 +1,6 @@
 trip=input("where do you want to go?")
 match trip:
-    case forest:
+    case "forest":
         choose=input("hide or walk")
         if choose == "hide":
             print("you hide behind a tree")
@@ -8,4 +8,16 @@ match trip:
             print("you find a sleeping wolf")
         else:
             print("unvalid forest action")
+    case cave:
+        choose=input("do you have a torch") 
+        if choose == "yes":
+            turn=input("left or right")
+            if turn == "left":
+                print("you find gold")
+            elif turn == "right":
+                print("you find bats")
+            else:
+                print("invalid cave path")
+        else:
+            print("its too dark to enter")             
 
